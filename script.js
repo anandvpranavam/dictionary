a9de3fc90d6d4d8f9912b35376f532083072e09d
Anand Venugopalapanicker <anandvpranavam@gmail.com>
2023-06-11 20:48:11 -0400

    added page formating and created dictionary webpage, learned adding id, order of appending, forEach usage etc

--- script.js
+++ script.js
@@ -22,32 +22,86 @@
 //   .catch((error) => error)
 // }
 
+
+function displayData(meaningResponse){
+  const firstElement = meaningResponse[0]
+  const mainDiv = document.getElementById("mainDiv")
+  mainDiv.innerHTML = "";
+  const wordDiv = document.createElement("div")
+  wordDiv.id="WordDiv";
+  wordDiv.innerText= "Word : "+firstElement.word
+  mainDiv.appendChild(wordDiv)
+  const meanings = document.createElement("div")
+  meanings.id="Meanings"
+  meanings.innerText = "Meanings"
+  mainDiv.appendChild(meanings)
+  firstElement.meanings.forEach((meaning, index)=>{
+    const pos = document.createElement("div")
+    pos.id="pos"+index;
+    meanings.appendChild(pos)
+    const posHeading = document.createElement("h2")
+    posHeading.innerText= "parts of speech: "+ meaning.partOfSpeech
+    console.log(meaning.partOfSpeech)
+    pos.appendChild(posHeading)
+    const definitionHeading = document.createElement("div")
+    definitionHeading.id = "Definitions"+index
+    definitionHeading.innerText= "Definitions"
+    pos.appendChild(definitionHeading)
+    const ul = document.createElement("ul")
+    ul.id = "definitionsData"+index
+    pos.appendChild(ul)
+    meaning.definitions.forEach((d)=>{
+      const liDefinitions = document.createElement("li")
+      liDefinitions.innerText = d.definition
+      ul.appendChild(liDefinitions)
+    })
+    const synonyms = document.createElement("div")
+    synonyms.id="Synonyms"+index
+    synonyms.innerText = "Synonyms: "+ meaning.synonyms
+    pos.appendChild(synonyms)
+  })
+
+
+
+}
 async function getMeaning(word) {
   try {
-    const laodingDiv = document.getElementById("loading")
-    laodingDiv.innerText = "loading..."
+    const loadingDiv = document.getElementById("loading")
+    loadingDiv.innerText = "loading..."
     const response = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
     if (response.status === 200) {
       const meaningResponse = await response.json()
-      const mainDiv = document.getElementById("mainDiv")
-      mainDiv.innerHTML = "";
-      const meanings = meaningResponse[0].meanings
-      meanings.forEach((meaning) => {
-        const posDiv = document.createElement("div")
-        posDiv.innerText = meaning.partOfSpeech
-        console.log(posDiv)
-        mainDiv.appendChild(posDiv)
-        const ul = document.createElement("ul")
-        meaning.definitions.forEach(d => {
-          const liDefinition = document.createElement("li")
-          liDefinition.innerText = d.definition
-          ul.appendChild(liDefinition)
-          // console.log(d.definition)
-        })
+       displayData(meaningResponse);
 
 
-        mainDiv.appendChild(ul)
-      })
+
+
+
+
+
+
+
+
+
+
+
+      // const meanings = meaningResponse[0].meanings
+      // meanings.forEach((meaning) => {
+      //   const posDiv = document.createElement("div")
+      //   posDiv.innerText = meaning.partOfSpeech
+      //   console.log(posDiv)
+      //   mainDiv.appendChild(posDiv)
+      //   const ul = document.createElement("ul")
+      //   meaning.definitions.forEach(d => {
+      //     const liDefinition = document.createElement("li")
+      //     liDefinition.innerText = d.definition
+      //     ul.appendChild(liDefinition)
+      //     // console.log(d.definition)
+      //   })
+
+
+      //   mainDiv.appendChild(ul)
+      // })
     } else {
       throw new Error("failed")
     }
